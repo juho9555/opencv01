cff315aa85ac19a2bf8f53de978355980f7a63b6
JUHO <ijuho9555@gmail.com>
2025-07-28 12:43:52 +0900

    이미지 크기변경
    cv2.resize()로 이미지 사이즈 변경
    단, image_small = cv2.resize('../img/like_lenna.png', (100,100))이 아니라 (image, (100,100))으로 해야됨

--- src/helloworld.py
+++ src/helloworld.py
@@ -1,7 +1,13 @@
 import cv2
 
 image = cv2.imread('../img/like_lenna.png')
-cv2.imshow('Image Window', image)
+image_small = cv2.resize(image, (500,500))
+
+#이미지를 보여주는 명령어
+#cv2.imshow('Image Window', image)
+cv2.imshow('Image Window', image_small)
+#print(image.shape)
+print(image_small.shape)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
