9b7a9b0d0d13338874742b45c55ecdd6080cff5c
JUHO <ijuho9555@gmail.com>
2025-07-28 13:04:52 +0900

    numpy 사용해서 데이터 수치 확인
    print(image.shape)을 입력하면 (224, 224, 3)으로 출력되는 것을 확인할 수 있다.

--- src/helloworld.py
+++ src/helloworld.py
@@ -1,13 +1,26 @@
 import cv2
+import numpy as np
+
+new_height = 300
+new_width = 300
 
 image = cv2.imread('../img/like_lenna.png')
-image_small = cv2.resize(image, (500,500))
+#image_small = cv2.resize(image, (500,500))
+
+
+dst = np.zeros((new_height, new_width), dtype=np.uint8)
+dst = cv2.resize(image, (new_width, new_height))
+
+cv2.resize(image, (new_width, new_height), dst=dst)
 
 #이미지를 보여주는 명령어
-#cv2.imshow('Image Window', image)
-cv2.imshow('Image Window', image_small)
-#print(image.shape)
-print(image_small.shape)
+cv2.imshow('Image Window', image)
+cv2.imshow('Image Window', dst)
+
+#cv2.imshow('Image Window', image_small)
+print(image.shape)
+#print(image_small.shape)
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
