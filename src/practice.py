20f255f1a297bd6c4011069658ae2b143966701b
JUHO <ijuho9555@gmail.com>
2025-07-28 13:14:21 +0900

    이미지를 배율로 사이즈 변환
    practice.py를 생성해 보기 쉽게 함

---
+++ src/practice.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+#이미지 가져오기
+image = cv2.imread('../img/like_lenna.png')
+
+#배율로 사이즈 변환
+image_big = cv2.resize(image, dsize=None, fx=2, fy=2)
+
+#이미지 보여줌
+cv2.imshow('Image_Window', image_big)
+
+#이미지 창 유지
+cv2.waitKey(0)
+cv2.destroyAllWindows()
